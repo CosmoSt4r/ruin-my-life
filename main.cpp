4c4693541d763c717afd10d36c9051b7d0228804
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-08-02 13:02:07 +0500

    Read/Write file funcs added

--- main.cpp
+++ main.cpp
@@ -1,10 +1,56 @@
 #include <iostream>
+#include <fstream>
+
+const char GREEK_QUESTION_MARK = ';', SPACE = ' ';
+
+void
+writeToFile(const std::string& filepath, const std::string& text)
+{
+    std::fstream file;
+	file.open(filepath, std::ios::out);
+	if (!file) 
+    {
+		std::cout << "Unable to create file at " << filepath << std::endl;
+        return;
+    }
+	else 
+		std::cout << "File created successfully at " << filepath << std::endl;
+
+	file << text;
+    file.close();
+}
+
+void
+readFromFile(const std::string& filepath, std::string& text)
+{
+    std::fstream file;
+	file.open(filepath, std::ios::in);
+    
+    if (!file) 
+    {
+		std::cout << "Unable to open file at " << filepath << std::endl;
+        return;
+    }
+	else 
+		std::cout << "File opened successfully at " << filepath << std::endl;
+
+    std::string line;
+    while (getline(file, line))
+        text += line + '\n';
+    file.close();
+}
+
+void
+ruinFile(const std::string& filepath)
+{
+    return;
+}
 
 int main(int argc, char *argv[])
 {
     for (int i = 1; i < argc; ++i)
     {
-        std::cout << argv[i] << std::endl;
+        ruinFile(argv[i]);
     }
 
     return 0;
